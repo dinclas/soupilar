6d895e602a028a060ce4de9650d88e7e92f1945c
Claudio Ramos <cramos@hired.com>
2024-05-15 18:40:41 -0300

    Add content-type validation

--- src/routers/words.py
+++ src/routers/words.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Depends, Request
 from pydantic import BaseModel
 from typing import List, Literal
 
@@ -6,6 +6,14 @@ from ..internal.vowels_counter import VowelsCounter
 from ..internal.words_sorter import WordsSorter
 
 router = APIRouter()
+
+
+# Return HTTP 415 if Content-Type is not application/json
+def validate_content_type(request: Request):
+    if request.headers.get("Content-Type") != "application/json":
+        raise HTTPException(
+            status_code=415, detail="Unsupported Media Type. Use application/json."
+        )
 
 
 class WordsPayload(BaseModel):
@@ -16,11 +24,11 @@ class SortPayload(WordsPayload):
     order: Literal["asc", "desc"]
 
 
-@router.post("/vowel_count")
+@router.post("/vowel_count", dependencies=[Depends(validate_content_type)])
 async def vowel_count(payload: WordsPayload):
     return {word: VowelsCounter.count_vowels(word) for word in payload.words}
 
 
-@router.post("/sort")
+@router.post("/sort", dependencies=[Depends(validate_content_type)])
 async def sort_words(payload: SortPayload):
     return WordsSorter.sort(payload.words, payload.order)
